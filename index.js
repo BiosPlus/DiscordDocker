1c63abffc61c720fe1ee35c5577c6879829d0521
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-02 04:34:57 +1000

    im crazy

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
 
 //Environment variable reading
 require('dotenv').config();
+
+/*
 const {
     prefix,
     token
 } = require('./config/config.json');
-
+*/
 
 //Start the Client
 const client = new Discord.Client();
@@ -16,11 +18,11 @@ const client = new Discord.Client();
 
 //lifecheck
 client.on('ready', async () => {
-    client.user.setActivity("Prefix: " + 'process.env.DISCORD_COMMAND_PREFIX', {
+    client.user.setActivity("Prefix: " + process.env.DISCORD_COMMAND_PREFIX, {
         type: "PLAYING"
     });
     console.log(client.user.username + ` is live.`);
 });
 
 //Client live
-client.login('process.env.DISCORD_AUTH_TOKEN');
+client.login(process.env.DISCORD_AUTH_TOKEN);
