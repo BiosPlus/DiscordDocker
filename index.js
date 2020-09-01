c2a6cec6ca7e866c3e73a04d9311e6ede0d636c3
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-02 04:30:03 +1000

    why you no work you fuck

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const client = new Discord.Client();
 
 //lifecheck
 client.on('ready', async () => {
-    client.user.setActivity("Prefix: " + prefix, {
+    client.user.setActivity("Prefix: " + 'process.env.DISCORD_COMMAND_PREFIX', {
         type: "PLAYING"
     });
     console.log(client.user.username + ` is live.`);
 });
 
 //Client live
-client.login(token);
+client.login('process.env.DISCORD_AUTH_TOKEN');
