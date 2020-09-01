891cf774f0a396a4dc54b47c09ddcce55b7d3e86
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-02 02:49:55 +1000

    grab old code

---
+++ index.js
@@ -0,0 +1,8 @@
+// Import the discord.js module
+const Discord = require('discord.js');
+const fs = require("fs");
+const {
+    prefix,
+    token,
+    ServerIP
+} = require('./config/config.json');
