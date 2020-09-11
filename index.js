f3f6277d27bd5f1688c7e2cdd02b545c235e3efd
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-12 01:40:34 +1000

    disabled login to test github actions

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ client.on('ready', async () => {
 });
 
 //Client login
-client.login(process.env.DISCORD_AUTH_TOKEN);
+//client.login(process.env.DISCORD_AUTH_TOKEN);
 
 //Cycles through function to test build was successful and then shuts down bot (so we don't rack up 1 million github minutes used)
+/*
 client.on('ready', () => {
+
     const channel = client.channels.cache.get('process.env.DISCORD_BUILD_CHANNEL')
     .then(console.log("I AM ALIVE! NOW TESTING TO SEE IF I CAN SEND A SUCCESSFUL MESSAGE!"))
     .then(channel.send('Build Successful!'))
     .then(client.destroy());
 });
+*/
