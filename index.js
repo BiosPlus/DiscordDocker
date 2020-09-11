82808abe268cf168d300f4f95318e0f533e5a756
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-12 03:20:13 +1000

    cleaned up index.js

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const client = new Discord.Client();
 client.on('ready', async () => {
     let enviro = process.env.CURRENT_ENVIRONMENT;
 
+    //Client login
+    client.login(process.env.DISCORD_AUTH_TOKEN);
     console.log(client.user.username + ` is live.`);
+
+    //What environment are we running in?
     console.log(`Currently running in `, enviro, ` mode.`);
     if (enviro === 'PRODUCTION') {
         console.log(`Ready for Commands`);
@@ -21,29 +25,14 @@ client.on('ready', async () => {
     }
     else {
         console.log(`Killing client process. Either in BUILD mode or you have not defined CURRENT_ENVIRONMENT`);
-        console.log(process.env.CURRENT_ENVIRONMENT);
-        client.destroy();
+        if (enviro === 'BUILD') {
+            let build_channel_id = process.env.DISCORD_BUILD_CHANNEL;
+            client.channels.cache.get(build_channel_id);
+            channel.send(`Build successful.`);
+            client.destroy();
+        }
+        else {
+            client.destroy();
+        }
     }
 });
-
-/*
-client.on('ready', async () => {
-    console.log(client.user.username + ` is live.`);
-    client.destroy();
-});
-*/
-
-//Client login
-client.login(process.env.DISCORD_AUTH_TOKEN);
-
-
-/*
-//Cycles through function to test build was successful and then shuts down bot (so we don't rack up 1 million github minutes used)
-client.on('ready', () => {
-
-    const channel = client.channels.cache.get('process.env.DISCORD_BUILD_CHANNEL')
-    .then(console.log("I AM ALIVE! NOW TESTING TO SEE IF I CAN SEND A SUCCESSFUL MESSAGE!"))
-    .then(channel.send('Build Successful!'))
-    .then(client.destroy());
-});
-*/
