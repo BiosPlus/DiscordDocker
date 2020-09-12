a396a5454bf0d8c6535fbc0e0dbba4e5328b859e
BiosPlus <9003059+BiosPlus@users.noreply.github.com>
2020-09-12 15:58:16 +1000

    tidying up

--- index.js
+++ index.js
@@ -4,30 +4,33 @@ const Discord = require('discord.js');
 //Environment variable reading
 require('dotenv').config();
 
+//ENV variables
+const current_environment = process.env.CURRENT_ENVIRONMENT;
+const build_channel_id = process.env.DISCORD_BUILD_CHANNEL;
+const discord_auth_token = process.env.DISCORD_AUTH_TOKEN;
+const discord_command_prefix = process.env.DISCORD_COMMAND_PREFIX;
+
 //Create a client instance
 const client = new Discord.Client();
-const token = process.env.DISCORD_AUTH_TOKEN;
 
 //Client login
-client.login(token);
+client.login(discord_auth_token);
 
 //Runs when the client instance is booted depending on the CURRENT_ENVIRONMENT variable.
 client.once('ready', () => {
-    let enviro = process.env.CURRENT_ENVIRONMENT;
     console.log(client.user.username + ` is live.`);
 
     //What environment are we running in?
-    console.log(`Currently running in `, enviro, ` mode.`);
-    if (enviro === 'PRODUCTION') {
+    console.log(`Currently running in `, current_environment, ` mode.`);
+    if (current_environment === 'PRODUCTION') {
         console.log(`Ready for Commands`);
     }
-    else if (enviro === 'DEVELOPMENT') {
+    else if (current_environment === 'DEVELOPMENT') {
         console.log(`Ready for commands`);
     }
     else {
         console.log(`Killing client process. Either in BUILD mode or you have not defined CURRENT_ENVIRONMENT`);
-        if (enviro === 'BUILD') {
-            let build_channel_id = process.env.DISCORD_BUILD_CHANNEL;
+        if (current_environment === 'BUILD') {
             const channel = client.channels.cache.get(build_channel_id);
             console.log(`Build successful.`);
             client.destroy();
